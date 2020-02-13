36b8f84644105adf1ead2ef50964742ad92ff3cd
smlu <smlu@s5.net>
2020-02-13 15:28:44 +0100

    Migrate android project to androidx and Android embedding v2

--- eosign_mobile_app/android/app/src/main/kotlin/zeropass/eosign_mobile_app/MainActivity.kt
+++ eosign_mobile_app/android/app/src/main/kotlin/zeropass/eosign_mobile_app/MainActivity.kt
@@ -1,13 +1,13 @@
 package zeropass.eosign_mobile_app
 
-import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
