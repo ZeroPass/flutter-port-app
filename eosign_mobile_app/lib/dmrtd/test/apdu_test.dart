da42165704ee55d41a4eb04bd31b7d670e8d4f40
smlu <smlu@s5.net>
2020-03-10 00:02:34 +0100

    Refactor (apdtu_test): Fix including & ResponseAPDU status test

--- eosign_mobile_app/lib/dmrtd/test/apdu_test.dart
+++ eosign_mobile_app/lib/dmrtd/test/apdu_test.dart
@@ -3,8 +3,8 @@
 import 'dart:typed_data';
 import 'package:test/test.dart';
 
-import 'package:dmrtd/src/com/command_apdu.dart';
-import 'package:dmrtd/src/com/response_apdu.dart';
+import 'package:dmrtd/src/proto/iso7816/command_apdu.dart';
+import 'package:dmrtd/src/proto/iso7816/response_apdu.dart';
 import 'package:dmrtd/src/extension/string_apis.dart';
 
 void main() {
@@ -22,52 +22,52 @@ void main() {
     // Test vectors from ICAO 9303 p11 appendix D.4
     // Test 1
     var rapdu = ResponseAPDU("990290008E08FA855A5D4C50A8ED9000".parseHex());
-    expect( rapdu.sw1  , 0x90 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , "990290008E08FA855A5D4C50A8ED".parseHex() );
+    expect( rapdu.status.sw1 , 0x90 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , "990290008E08FA855A5D4C50A8ED".parseHex() );
 
     // Test 2
     rapdu = ResponseAPDU("8709019FF0EC34F9922651990290008E08AD55CC17140B2DED9000".parseHex());
-    expect( rapdu.sw1  , 0x90 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED".parseHex() );
+    expect( rapdu.status.sw1 , 0x90 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , "8709019FF0EC34F9922651990290008E08AD55CC17140B2DED".parseHex() );
 
     // Test 3
     rapdu = ResponseAPDU("871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D749000".parseHex());
-    expect( rapdu.sw1  , 0x90 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D74".parseHex() );
+    expect( rapdu.status.sw1 , 0x90 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , "871901FB9235F4E4037F2327DCC8964F1F9B8C30F42C8E2FFF224A990290008E08C8B2787EAEA07D74".parseHex() );
   
     // Test response status word
     rapdu = ResponseAPDU("9000".parseHex());
-    expect( rapdu.sw1  , 0x90 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0x90 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , null );
 
     rapdu = ResponseAPDU("6A80".parseHex());
-    expect( rapdu.sw1  , 0x6A );
-    expect( rapdu.sw2  , 0x80 );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0x6A );
+    expect( rapdu.status.sw2 , 0x80 );
+    expect( rapdu.data       , null );
 
     rapdu = ResponseAPDU("6A88".parseHex());
-    expect( rapdu.sw1  , 0x6A );
-    expect( rapdu.sw2  , 0x88 );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0x6A );
+    expect( rapdu.status.sw2 , 0x88 );
+    expect( rapdu.data       , null );
 
     rapdu = ResponseAPDU("6300".parseHex());
-    expect( rapdu.sw1  , 0x63 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0x63 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , null );
 
     rapdu = ResponseAPDU(Uint8List(2));
-    expect( rapdu.sw1  , 0x00 );
-    expect( rapdu.sw2  , 0x00 );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0x00 );
+    expect( rapdu.status.sw2 , 0x00 );
+    expect( rapdu.data       , null );
 
     rapdu = ResponseAPDU("FFFF".parseHex());
-    expect( rapdu.sw1  , 0xFF );
-    expect( rapdu.sw2  , 0xFF );
-    expect( rapdu.data , null );
+    expect( rapdu.status.sw1 , 0xFF );
+    expect( rapdu.status.sw2 , 0xFF );
+    expect( rapdu.data       , null );
 
     expect( () =>  ResponseAPDU(null), throwsArgumentError );
     expect( () =>  ResponseAPDU(Uint8List(0)), throwsArgumentError );
