7931520ffb3919eb8dc69036729be5a428b1e17a
smlu <smlu@s5.net>
2020-03-12 02:44:12 +0100

    Add intToBin utility function

--- eosign_mobile_app/lib/dmrtd/lib/src/utils.dart
+++ eosign_mobile_app/lib/dmrtd/lib/src/utils.dart
@@ -1,5 +1,6 @@
 //  Created by smlu, copyright © 2020 ZeroPass. All rights reserved.
 import 'dart:math';
+import 'dart:typed_data';
 
 class Utils {
 
@@ -19,5 +20,22 @@ class Utils {
   /// [n] must be positive integer number.
   static int byteCount(final int n) {
     return (bitCount(n) / 8).ceil();
+  }
+
+  /// Returns serialized [n] in big endian byte order and shrunk to the first set byte or [minLen]. 
+  /// If [minLen] is 0 and [n] is 0, empty [Uint8List] will be returned. 
+  static Uint8List intToBin(int n, {int minLen = 1}) {
+    assert(minLen >= 0);
+    final raw = Uint8List(8);
+    ByteData.view(raw.buffer).setUint64(0, n, Endian.big);
+
+    // get first set byte
+    int i = 0;
+    for(; i < raw.length; i++) {
+      if(raw[i] != 0x00 ) {
+        break;
+      }
+    }
+    return raw.sublist( raw.length - i >= minLen ? i : raw.length  - minLen);
   }
 }
