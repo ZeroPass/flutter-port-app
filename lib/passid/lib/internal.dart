7e7b4f6b48b417c22b4f9e5670e8ac4c7de4d404
smlu <smlu@s5.net>
2020-04-06 13:30:00 +0200

    Add pass Id library headers

---
+++ lib/passid/lib/internal.dart
@@ -0,0 +1,6 @@
+//  Created by smlu, copyright © 2020 ZeroPass. All rights reserved.
+export 'src/proto/passid_api.dart';
+export 'src/proto/rpc/jrpc.dart';
+export 'src/proto/rpc/jrpc_objects.dart';
+export 'src/proto/session.dart';
+export 'src/proto/session_key.dart';
---
+++ lib/passid/lib/passid.dart
@@ -0,0 +1,9 @@
+//  Created by smlu, copyright © 2020 ZeroPass. All rights reserved.
+library passid;
+
+export 'src/authn_data.dart';
+export 'src/passid_client.dart';
+export 'src/proto/challenge_signature.dart';
+export 'src/proto/passid_error.dart';
+export 'src/proto/proto_challenge.dart';
+export 'src/proto/uid.dart';
