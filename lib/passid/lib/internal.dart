f011b384a9a65683682cd54e8b08ba44a2186a0b
Nejc Skerjanc <ns0280@student.uni-lj.si>
2020-12-28 10:31:17 +0100

    passid folder is deleted; it needs to be downloaded separately from passid project

--- lib/passid/lib/internal.dart
+++
@@ -1,6 +0,0 @@
-//  Created by smlu, copyright © 2020 ZeroPass. All rights reserved.
-export 'src/proto/passid_api.dart';
-export 'src/proto/rpc/jrpc.dart';
-export 'src/proto/rpc/jrpc_objects.dart';
-export 'src/proto/session.dart';
-export 'src/proto/session_key.dart';
--- lib/passid/lib/passid.dart
+++
@@ -1,9 +0,0 @@
-//  Created by smlu, copyright © 2020 ZeroPass. All rights reserved.
-library passid;
-
-export 'src/authn_data.dart';
-export 'src/passid_client.dart';
-export 'src/proto/challenge_signature.dart';
-export 'src/proto/passid_error.dart';
-export 'src/proto/proto_challenge.dart';
-export 'src/proto/uid.dart';
